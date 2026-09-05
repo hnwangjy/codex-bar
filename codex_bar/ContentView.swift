e4230afd9c165003519d42beb7e82960f2b6f2d1
hnwangjy <hnwangjy@hotmail.com>
2026-09-05 09:20:42 +0800

    Initial Commit

---
+++ codex_bar/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  codex_bar
+//
+//  Created by wjy on 2026/9/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ codex_bar/codex_barApp.swift
@@ -0,0 +1,17 @@
+//
+//  codex_barApp.swift
+//  codex_bar
+//
+//  Created by wjy on 2026/9/5.
+//
+
+import SwiftUI
+
+@main
+struct codex_barApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
